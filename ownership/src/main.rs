64d050b7c4fe2ae2b49212c14a431b6420038309
mdmahendri <mahendri.md@gmail.com>
2026-01-16 22:25:58 +0700

    ch4 done

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -23,9 +23,16 @@ fn main() {
     println!("{r1}");
     let r2 = &mut s5;
     r2.push_str(", world");
-    // code below will throw compilation error
+    // code below will trigger compilation error
     // cannot borrow `s5` as mutable because it is also borrowed as immutable
     // println!("{r1}");
+
+    let mut s6 = String::from("hello world");
+    let word = first_word(&s6);
+    // code below will trigger compilation error
+    // cannot borrow `s6` as mutable because it is also borrowed as immutable
+    // s6.clear();
+    println!("the first word is: {word}");
 }
 
 fn calculate_length(s: &String) -> usize {
@@ -34,4 +41,15 @@ fn calculate_length(s: &String) -> usize {
 
 fn change_ref(s: &mut String) {
     s.push_str(", world");
+}
+
+fn first_word(s: &str) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
