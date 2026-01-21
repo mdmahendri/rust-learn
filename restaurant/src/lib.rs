4c9f902d3c9a13f0edfb90c671723ca61f6c2383
mdmahendri <mahendri.md@gmail.com>
2026-01-21 07:04:55 +0700

    ch7 done

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -3,18 +3,7 @@
 // use std::io::Write;
 use std::io::{self, Write};
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
+mod front_of_house;
 
 mod customer {
     use crate::front_of_house::hosting;
