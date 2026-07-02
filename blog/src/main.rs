9d5bb297b6f91c0ee9fc0a7fb173277566b6625e
mdmahendri <mahendri.md@gmail.com>
2026-07-03 06:05:31 +0700

    ch18 part2

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -4,4 +4,8 @@ fn main() {
     let mut post = Post::new();
     post.add_text("now test 123");
     assert_eq!("", post.content());
+    post.request_review();
+    assert_eq!("", post.content());
+    post.approve();
+    assert_eq!("now test 123", post.content());
 }
