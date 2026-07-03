6e9efc88e7a5e2187dc1ae1eb400da583668bbd5
mdmahendri <mahendri.md@gmail.com>
2026-07-03 07:21:54 +0700

    ch18 done

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -3,9 +3,7 @@ use blog::Post;
 fn main() {
     let mut post = Post::new();
     post.add_text("now test 123");
-    assert_eq!("", post.content());
-    post.request_review();
-    assert_eq!("", post.content());
-    post.approve();
+    let post = post.request_review();
+    let post = post.approve();
     assert_eq!("now test 123", post.content());
 }
